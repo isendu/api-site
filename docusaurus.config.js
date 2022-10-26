83a3d245d828874b37deace714f156a2ce65021d
Edoardo Pacciani <edoardo.pacciani@isendu.com>
2022-10-26 17:59:35 +0200

    feat: landing + navbar config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,14 +63,12 @@ const config = {
         },
         items: [
           {
-            type: "doc",
-            docId: "intro",
+            to: "/docs/api/store-api",
             position: "left",
-            label: "Reference",
+            label: "Store API",
           },
-          { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/isendu/api-site",
             label: "GitHub",
             position: "right",
           },
@@ -78,48 +76,8 @@ const config = {
       },
       footer: {
         style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Reference",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        links: [{ label: "isendu.com", href: "https://www.isendu.com" }],
+        copyright: `Copyright © ${new Date().getFullYear()} isendu API, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,5 @@
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import Layout from "@theme/Layout";
 import React from "react";
-
+import { Redirect } from "@docusaurus/router";
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+  return <Redirect to="/docs/api/store-api" />;
 }
