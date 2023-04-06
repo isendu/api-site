10884d2c5e7542ae66a5303168a62c6ad42fdfa6
Edoardo Pacciani <edoardo.pacciani@isendu.com>
2023-04-06 18:07:54 +0200

    Fix config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: "Isendu are cool",
   url: "https://publicapi.isendu.dev",
   baseUrl: "/",
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
 
