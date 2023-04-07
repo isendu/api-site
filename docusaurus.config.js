3affe1df36dc9a2aae5746494e63319d4a3c8d8e
Edoardo Pacciani <edoardo.pacciani@isendu.com>
2023-04-07 12:41:55 +0200

    fix: config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Isendu API",
-  url: "https://isendu.github.io",
+  url: "https://apidocs.isendu.com",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
